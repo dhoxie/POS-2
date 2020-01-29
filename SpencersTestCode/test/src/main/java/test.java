ebfcbce1d3e46f5fdffa6fb25c5932ea073b0454
Spencer <Curley>
2020-01-29 09:09:56 -0800

    added test code to show proof of consept that we can use external java to make pdf's

---
+++ SpencersTestCode/test/src/main/java/test.java
@@ -0,0 +1,17 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.io.FileNotFoundException;
+
+public class test {
+    public static void main(String [] args) throws FileNotFoundException {
+        PdfWriter writer = new  PdfWriter("test.pdf");
+        PdfDocument pdf = new PdfDocument(writer);
+        Document document = new Document(pdf);
+        document.add(new Paragraph("Hello World!"));
+        document.close();
+    }
+
+}
