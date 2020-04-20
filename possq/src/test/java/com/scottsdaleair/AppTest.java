509ebe5992aa5c9c596a37a5d3116de2180747b0
Luke Mattfeld <lukemattfeld@gmail.com>
2020-04-20 13:57:37 -0700

    added TestFX as dependency

--- possq/src/test/java/com/scottsdaleair/AppTest.java
+++ possq/src/test/java/com/scottsdaleair/AppTest.java
@@ -1,38 +1,45 @@
 package com.scottsdaleair;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit.ApplicationTest;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+import com.sun.tools.javac.Main;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
+import javafx.stage.Stage;
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest extends ApplicationTest {
+
+  @Override
+  public void start (Stage stage) throws Exception {
+    Parent mainNode = FXMLLoader.load(Main.class.getResource("sample.fxml"));
+    stage.setScene(new Scene(mainNode));
+    stage.show();
+    stage.toFront();
+  }
+
+  @Before
+  public void setUp () throws Exception {
+  }
+
+  @After
+  public void tearDown () throws Exception {
+    FxToolkit.hideStage();
+    release(new KeyCode[]{});
+    release(new MouseButton[]{});
+  }
+
+  @Test
+  public void testEnglishInput () {
+    clickOn("#inputField");
+    write("This is a test!");
+  }
+
 }
