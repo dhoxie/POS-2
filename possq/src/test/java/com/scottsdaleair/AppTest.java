4b945f290c17f9a8ec0c1979f0bb267724e92e6a
Luke Mattfeld <lukemattfeld@gmail.com>
2020-05-02 23:00:23 -0700

    Changes for TestFX Framework
    Changed up the App class structure to accomadate for a simpler FX tester,
    Added testing framework. Still getting errors

--- possq/src/test/java/com/scottsdaleair/AppTest.java
+++ possq/src/test/java/com/scottsdaleair/AppTest.java
@@ -1,45 +1,63 @@
 package com.scottsdaleair;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import static org.testfx.api.FxAssert.verifyThat;
+import static org.testfx.matcher.control.LabeledMatchers.hasText;
+
+import java.io.File;
+import java.net.URL;
+
 import org.testfx.api.FxToolkit;
 import org.testfx.framework.junit.ApplicationTest;
+import org.testfx.robot.Motion;
 
-import com.sun.tools.javac.Main;
-
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 public class AppTest extends ApplicationTest {
 
-  @Override
-  public void start (Stage stage) throws Exception {
-    Parent mainNode = FXMLLoader.load(Main.class.getResource("sample.fxml"));
-    stage.setScene(new Scene(mainNode));
-    stage.show();
-    stage.toFront();
-  }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+//		primaryStage.setTitle(window.TITLE);
+//		primaryStage.setScene(window.getScene());
+//		primaryStage.show();
+		new App().start(primaryStage);
+	}
+	
+//	@Test public void test_should_test() {
+//		clickOn("Point of Sale");
+//	}
 
-  @Before
-  public void setUp () throws Exception {
-  }
+	@Before
+	public void setUp() throws Exception {
+		FxToolkit.registerPrimaryStage();
+		FxToolkit.setupApplication(App.class);
+	}
 
-  @After
-  public void tearDown () throws Exception {
-    FxToolkit.hideStage();
-    release(new KeyCode[]{});
-    release(new MouseButton[]{});
-  }
-
-  @Test
-  public void testEnglishInput () {
-    clickOn("#inputField");
-    write("This is a test!");
-  }
+//	@After
+//	public void tearDown() throws Exception {
+//		FxToolkit.hideStage();
+//		release(new KeyCode[] {});
+//		release(new MouseButton[] {});
+//	}
+//	
+	@Test
+	public void has_pos_navbar() {
+//		TextField tfieldFirstName = find("#txt_FirstNameSearch");
+		clickOn("#txt_FirstNameSearch");
+		System.out.println("Clicked on field");
+		write("This is a test!");
+	
+//		clickOn("")
+	}
 
 }
