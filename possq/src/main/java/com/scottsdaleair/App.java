e8fe9856ef4193cee76773c52f2606921c36402d
scc128 <spencercc128@yahoo.com>
2020-05-12 12:10:25 -0700

    added config class and a test config file

--- possq/src/main/java/com/scottsdaleair/App.java
+++ possq/src/main/java/com/scottsdaleair/App.java
@@ -2,17 +2,32 @@ package com.scottsdaleair;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import com.scottsdaleair.utils.Config;
 public class App extends Application {
   private final POSWindow window = new POSWindow();
-
+  private static Config theConfig; 
   @Override
   public void start(Stage primaryStage) throws Exception {
+    System.out.println("testing config before ");
+   
     primaryStage.setTitle(window.title);
     primaryStage.setScene(window.getScene());
     primaryStage.show();
+    System.out.println("testing config after ");
+    theConfig = Config.getConfig();
+    if(theConfig != null){
+      System.out.println(theConfig.getPrimaryDatabaseIp());
+      theConfig.changePrimaryDatabase("thisis the new ip" , "this is the newport ");
+    }else{
+      System.out.println("was null");
+    }
+    
   }
-
+  @Override 
+  public void stop(){
+    theConfig.finalize();
+    theConfig = null;
+  }
 
   public static void main(String[] args) {
     launch(args);
