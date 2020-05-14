43ca62515d84d696b117b2ec1cea0ed461018698
spencer <scurley2@eagles.ewu.edu>
2020-05-14 13:55:41 -0700

    changes fixed for checkstyle import and int types

--- possq/src/main/java/com/scottsdaleair/utils/Config.java
+++ possq/src/main/java/com/scottsdaleair/utils/Config.java
@@ -2,90 +2,102 @@
 // primaryIp,primaryport 
 // secondaryIp,secondaryPort
 // emailPassword 
+
 package com.scottsdaleair.utils;
-import java.io.*;
-import java.util.*;
+
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.File;
+
 public class Config {
-    private static Config theConfig;
-    private String primaryDatabaseIp;
-    private String secondaryDatabaseIp;
-    private String primaryDatabasePort;
-    private String secondaryDatabasePort;
-    private String emailPassword; 
-    private Boolean changed;
-    private final String filename = "config.scottsdaleAirConfig";
-    private Config() throws Exception{
-        // open config file here 
-        this.changed = false;
-        Scanner configFileScanner = new Scanner(new File(filename) );
-        String primary = configFileScanner.nextLine(); 
-        String secondary = configFileScanner.nextLine();
-        this.emailPassword = configFileScanner.nextLine();
-        String [] databaseTmp = primary.split(",");
-        this.primaryDatabaseIp = databaseTmp[0];
-        this.primaryDatabasePort = databaseTmp [1];
-        databaseTmp = secondary.split(",");
-        this.secondaryDatabaseIp = databaseTmp[0];
-        this.secondaryDatabasePort = databaseTmp[1];
+  private static Config theConfig;
+  private String primaryDatabaseIp;
+  private String secondaryDatabaseIp;
+  private int primaryDatabasePort;
+  private int secondaryDatabasePort;
+  private String emailPassword;
+  private Boolean changed;
+  private final String filename = "config.scottsdaleAirConfig";
 
+  private Config() throws Exception {
+    // open config file here
+    this.changed = false;
+    Scanner configFileScanner = new Scanner(new File(filename));
+    String primary = configFileScanner.nextLine();
+    String[] databaseTmp = primary.split(",");
+    this.primaryDatabaseIp = databaseTmp[0];
+    this.primaryDatabasePort = (int) Integer.parseInt(databaseTmp[1]);
+    String secondary = configFileScanner.nextLine();
+    this.emailPassword = configFileScanner.nextLine();
+    databaseTmp = secondary.split(",");
+    this.secondaryDatabaseIp = databaseTmp[0];
+    this.secondaryDatabasePort = (int) Integer.parseInt(databaseTmp[1]);
+
+  }
+
+  // singleton
+  public static Config getConfig() {
+    if (theConfig == null) {
+      try {
+        return new Config();
+      } catch (Exception e) {
+        System.out.println("Error in creating config from file");
+        return null;
+      }
+    } else {
+      return theConfig;
     }
-    // singleton 
-    public static Config getConfig(){
-        if(theConfig == null){
-            try{   
-                return new Config();
-            }catch(Exception e )
-            {
-                System.out.println("Error in creating config from file");
-                return null;
-            }
-        }else{
-            return theConfig;
-        }
-    }
-    public void changePrimaryDatabase(String newIp , String newPort){
-        this.changed = true;
-        this.primaryDatabaseIp = newIp;
-        this.primaryDatabasePort = newPort;
-    }
-    public String getPrimaryDatabaseIp(){
-        return this.primaryDatabaseIp;
-    }
-    public String getPrimaryDatabasePort(){
-        return this.primaryDatabasePort;
-    }
-    public void changeSecondaryDatabase(String newIp , String newPort){
-        this.changed = true;
-        this.secondaryDatabaseIp = newIp;
-        this.secondaryDatabasePort = newPort;
-    }
-    public String getSecondaryDatabaseIp(){
-        return this.secondaryDatabaseIp;
-    }
-    public String getSecondaryDatabasePort(){
-        return this.secondaryDatabasePort;
-    }
-    //there is no change email password or change from email because that would require rewiring all of the email class. 
-    // maybe will do that in the future but not now. 
-    // it is not as easy to change as the ip/port of the database 
-    public String getEmailPassword(){
-        return this.emailPassword;
+  }
+
+  public void changePrimaryDatabase(String newIp, int newPort) {
+    this.changed = true;
+    this.primaryDatabaseIp = newIp;
+    this.primaryDatabasePort = newPort;
+  }
+
+  public String getPrimaryDatabaseIp() {
+    return this.primaryDatabaseIp;
+  }
+
+  public int getPrimaryDatabasePort() {
+    return this.primaryDatabasePort;
+  }
+
+  public void changeSecondaryDatabase(String newIp, int newPort) {
+    this.changed = true;
+    this.secondaryDatabaseIp = newIp;
+    this.secondaryDatabasePort = newPort;
+  }
+
+  public String getSecondaryDatabaseIp() {
+    return this.secondaryDatabaseIp;
+  }
+
+  public int getSecondaryDatabasePort() {
+    return this.secondaryDatabasePort;
+  }
+
+  // there is no change email password or change from email because that would
+  // require rewiring all of the email class.
+  // maybe will do that in the future but not now.
+  // it is not as easy to change as the ip/port of the database
+  public String getEmailPassword() {
+    return this.emailPassword;
+  }
+
+  public void finalize() {
+    System.out.println("finalized called ");
+    if (changed) {
+      try {
+        FileWriter output = new FileWriter(filename);
+        output.write(this.primaryDatabaseIp + "," + this.primaryDatabasePort + "\n" 
+            + this.secondaryDatabaseIp + ","
+            + this.secondaryDatabasePort + "\n" + this.emailPassword + "\n");
+        output.close();
+      } catch (Exception e) {
+        System.out.println("did not write changes to the config file ");
+      }
     }
 
-    public void finalize(){
-        System.out.println("finalized called ");
-        if(changed){
-            try{
-                FileWriter output = new FileWriter(filename);
-                output.write(this.primaryDatabaseIp + "," + this.primaryDatabasePort + "\n"
-                    + this.secondaryDatabaseIp + "," + this.secondaryDatabasePort + "\n" 
-                    + this.emailPassword+"\n");
-                output.close();
-            }catch(Exception e ){
-                System.out.println("did not write changes to the config file ");
-            }
-        }
-           
-    }
+  }
 }
-
--- possq/src/main/java/com/scottsdaleair/utils/DatabaseUtils.java
+++ possq/src/main/java/com/scottsdaleair/utils/DatabaseUtils.java
@@ -11,13 +11,14 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import org.bson.Document;
 import com.scottsdaleair.utils.Config; 
+
 public class DatabaseUtils {
 
   private static final String dbAddr = Config.getConfig().getPrimaryDatabaseIp();
-  private static final int dbPort = Integer.parseInt(Config.getConfig().getPrimaryDatabasePort());
+  private static final int dbPort = Config.getConfig().getPrimaryDatabasePort();
   public static final String dbName = "userdat";
   public static final String backupDbAddress = Config.getConfig().getSecondaryDatabaseIp();
-  public static final int backupDbPort = Integer.parseInt(Config.getConfig().getSecondaryDatabasePort());
+  public static final int backupDbPort = Config.getConfig().getSecondaryDatabasePort();
   private static MongoClient client;
   private static final Collation collation =
       Collation.builder().locale("en").collationStrength(CollationStrength.SECONDARY).build();
@@ -25,14 +26,14 @@ public class DatabaseUtils {
   static {
     try {
       client = new MongoClient(dbAddr, dbPort);
-    } catch (Exception e) {
+    } catch (final Exception e) {
       client = null;
     }
     if (client == null) {
       try {
         client = null;
         client = new MongoClient(backupDbAddress, backupDbPort);
-      } catch (Exception e) {
+      } catch (final Exception e) {
         e.printStackTrace();
         throw new RuntimeException("Exception occured in creating singleton instance");
       }
@@ -45,10 +46,10 @@ public class DatabaseUtils {
    * @param collection Collection to add object to
    * @param obj        The object to be added
    */
-  public static void addObjToCollection(MongoCollection<Document> collection, Object obj) {
-    Gson gson = new Gson();
-    String obJson = gson.toJson(obj);
-    Document newDoc = Document.parse(obJson);
+  public static void addObjToCollection(final MongoCollection<Document> collection, final Object obj) {
+    final Gson gson = new Gson();
+    final String obJson = gson.toJson(obj);
+    final Document newDoc = Document.parse(obJson);
     collection.insertOne(newDoc);
   }
 
@@ -58,7 +59,7 @@ public class DatabaseUtils {
    * @param collectionName The name of the collection
    * @param obj            The object to be added
    */
-  public static void addObjToCollection(String collectionName, Object obj) {
+  public static void addObjToCollection(final String collectionName, final Object obj) {
     addObjToDb(DatabaseUtils.dbName, collectionName, obj);
   }
 
@@ -69,29 +70,29 @@ public class DatabaseUtils {
    * @param collectionName The name of the collection
    * @param obj            The object to be added
    */
-  private static void addObjToDb(String dbname, String collectionName, Object obj) {
-    MongoDatabase database = DatabaseUtils.client.getDatabase(dbname);
-    MongoCollection<Document> collection = database.getCollection(collectionName);
+  private static void addObjToDb(final String dbname, final String collectionName, final Object obj) {
+    final MongoDatabase database = DatabaseUtils.client.getDatabase(dbname);
+    final MongoCollection<Document> collection = database.getCollection(collectionName);
     addObjToCollection(collection, obj);
   }
 
-  private static Object[] searchCollection(MongoCollection<Document> collection, String key,
-      String value, Type t) {
-    Gson gson = new Gson();
-    Document match = new Document(key, value);
-    FindIterable<Document> results = collection.find(match).collation(collation);
-    ArrayList<Object> retListAr = new ArrayList<>();
-    for (Document d : results) {
+  private static Object[] searchCollection(final MongoCollection<Document> collection, final String key,
+      final String value, final Type t) {
+    final Gson gson = new Gson();
+    final Document match = new Document(key, value);
+    final FindIterable<Document> results = collection.find(match).collation(collation);
+    final ArrayList<Object> retListAr = new ArrayList<>();
+    for (final Document d : results) {
       retListAr.add(gson.fromJson(d.toJson(), t));
     }
     return retListAr.toArray();
   }
 
-  private static Object[] retrieveCollection(MongoCollection<Document> collection, Type t) {
-    Gson gson = new Gson();
-    FindIterable<Document> results = collection.find();
-    ArrayList<Object> retListAr = new ArrayList<>();
-    for (Document d : results) {
+  private static Object[] retrieveCollection(final MongoCollection<Document> collection, final Type t) {
+    final Gson gson = new Gson();
+    final FindIterable<Document> results = collection.find();
+    final ArrayList<Object> retListAr = new ArrayList<>();
+    for (final Document d : results) {
       retListAr.add(gson.fromJson(d.toJson(), t));
     }
     return retListAr.toArray();
@@ -106,8 +107,8 @@ public class DatabaseUtils {
    * @param t              Type of object returned
    * @return Object[] containing search results
    */
-  public static Object[] getFromCollection(String collectionName, String key, String value,
-      Type t) {
+  public static Object[] getFromCollection(final String collectionName, final String key, final String value,
+      final Type t) {
 
     return getFromDb(DatabaseUtils.dbName, collectionName, key, value, t);
   }
@@ -119,16 +120,16 @@ public class DatabaseUtils {
    * @param t              Type of class to retrieve
    * @return Object[] containing search results
    */
-  public static Object[] getEntireCollection(String collectionName, Type t) {
-    MongoCollection<Document> collection =
+  public static Object[] getEntireCollection(final String collectionName, final Type t) {
+    final MongoCollection<Document> collection =
         DatabaseUtils.client.getDatabase(DatabaseUtils.dbName).getCollection(collectionName);
     return retrieveCollection(collection, t);
   }
 
-  private static Object[] getFromDb(String dbname, String collectionName, String key, String value,
-      Type t) {
-    MongoDatabase database = DatabaseUtils.client.getDatabase(dbname);
-    MongoCollection<Document> collection = database.getCollection(collectionName);
+  private static Object[] getFromDb(final String dbname, final String collectionName, final String key, final String value,
+      final Type t) {
+    final MongoDatabase database = DatabaseUtils.client.getDatabase(dbname);
+    final MongoCollection<Document> collection = database.getCollection(collectionName);
     return searchCollection(collection, key, value, t);
   }
 }
